0851267fd8166c0b967ac76ab8684d0338dbe51a
DaddyLomLom <shalombloodman02@gmail.com>
2026-02-14 11:24:03 -0400

    more changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUw6tMSV044z_9dWUK569wcsbDH7p2ydw",
@@ -16,3 +17,4 @@ const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
